6481104d26415b1cbe1a0f20d457f6b9138cf506
shrjal313 <shrijalshrestha313@gmail.com>
2021-05-18 14:00:04 +0545

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:day2/pages/product_detail/screens/product_detail.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductDetail(),
+    );
+  }
+}
---
+++ lib/pages/product_detail/screens/product_detail.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Row(
+                children: [],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
